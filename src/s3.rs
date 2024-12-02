e2efc3149c4840bcd6cb3a398a9f5efdc3163136
Dyno Fu <dfu@salesforce.com>
2024-12-02 09:00:05 -0800

    refactor: add context

--- src/s3.rs
+++ src/s3.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 use tracing::instrument;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use aws_config::{meta::region::RegionProviderChain, SdkConfig};
 use aws_sdk_s3::{config::Builder as S3Builder, primitives::ByteStream, Client};
 use aws_types::region::Region;
@@ -12,21 +12,34 @@ pub struct Key {
     pub key: String,
 }
 
+/// Get an object from S3 and write it to a local file
 #[instrument(skip(s3))]
 pub async fn get(s3: &Client, f: &Path, o: &Key) -> Result<()> {
-    let req = s3.get_object().bucket(&o.bucket).key(&o.key).send().await?;
+    let req = s3
+        .get_object()
+        .bucket(&o.bucket)
+        .key(&o.key)
+        .send()
+        .await
+        .with_context(|| format!("Failed to get object s3://{}/{}", o.bucket, o.key))?;
 
     let body = req.body;
-    let bytes = body.collect().await?;
+    let bytes = body
+        .collect()
+        .await
+        .context("Failed to collect object bytes from stream")?;
 
-    std::fs::write(f, bytes.into_bytes())?;
+    std::fs::write(f, bytes.into_bytes())
+        .with_context(|| format!("Failed to write object to file: {}", f.display()))?;
 
     Ok(())
 }
 
+/// Put a local file to S3
 #[instrument(skip(s3))]
 pub async fn put(s3: &Client, f: &Path, o: &Key) -> Result<()> {
-    let contents = std::fs::read(f)?;
+    let contents =
+        std::fs::read(f).with_context(|| format!("Failed to read file: {}", f.display()))?;
 
     let body = ByteStream::from(contents);
 
@@ -35,22 +48,26 @@ pub async fn put(s3: &Client, f: &Path, o: &Key) -> Result<()> {
         .key(&o.key)
         .body(body)
         .send()
-        .await?;
+        .await
+        .with_context(|| format!("Failed to put object to s3://{}/{}", o.bucket, o.key))?;
 
     Ok(())
 }
 
+/// Delete an object from S3
 #[instrument(skip(s3))]
 pub async fn del(s3: &Client, o: &Key) -> Result<()> {
     s3.delete_object()
         .bucket(&o.bucket)
         .key(&o.key)
         .send()
-        .await?;
+        .await
+        .with_context(|| format!("Failed to delete object s3://{}/{}", o.bucket, o.key))?;
 
     Ok(())
 }
 
+/// Rename an object in S3
 #[instrument(skip(s3))]
 pub async fn rename(s3: &Client, from: &Key, to: &Key) -> Result<()> {
     // Copy the object
@@ -59,7 +76,13 @@ pub async fn rename(s3: &Client, from: &Key, to: &Key) -> Result<()> {
         .bucket(to.bucket.clone())
         .key(to.key.clone())
         .send()
-        .await?;
+        .await
+        .with_context(|| {
+            format!(
+                "Failed to copy object from s3://{}/{} to s3://{}/{}",
+                from.bucket, from.key, to.bucket, to.key
+            )
+        })?;
 
     // Delete the original
     del(s3, from).await?;
